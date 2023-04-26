c0882114576f2e2f833d6613c28b7c0149422a3b
hlbrand <hlbrand2995@gmail.com>
2023-04-26 19:50:19 +0700

    02 - Connect to DB, setup Tailwind, New Product

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    DB_URI: 'mongodb+srv://admin:admin@cluster0.f2odcvj.mongodb.net/buyitnow',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
