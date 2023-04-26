5f0426e339bbd8a9ef64747b8d25933efbb02449
hlbrand <hlbrand2995@gmail.com>
2023-04-27 06:35:19 +0700

    03 - Get all Products, Homepage, Header

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   env: {
+    API_URL: 'http://localhost:3000',
     DB_URI: 'mongodb+srv://admin:admin@cluster0.f2odcvj.mongodb.net/buyitnow',
   },
 };
